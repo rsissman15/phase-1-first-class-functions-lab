7aab74127b4ddf1c704ee42624003d961f36c94e
Rebecca Sissman <rsissman15@aol.com>
2022-01-11 16:15:22 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const drivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function(multiplier){
+       return multiplier*integer;
+    }
+}
+
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+
+function selectDifferentDrivers(arrayOfDrivers,driverfunction){
+    return driverfunction(arrayOfDrivers);
+}
